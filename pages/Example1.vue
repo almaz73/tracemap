5471a231281f4a00c3c5184c4729ce9d56091dcd
almaz73 <almaz_73@mail.ru>
2018-07-16 16:35:58 +0300

    example1

---
+++ pages/Example1.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      First example
+      <div id="area"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Example1"
+    }
+</script>
+
+<style scoped>
+  #area{
+    display: flex;
+    width: 100vh
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,14 +9,7 @@
         Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <a href="/example1">Example1</a>
       </div>
     </div>
   </section>
