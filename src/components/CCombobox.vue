0f649fa15994b7b10070c1b44879d76c531024e1
almaz73 <almaz_73@mail.ru>
2018-08-24 09:22:33 +0300

    - комбобокс

--- src/components/CCombobox.vue
+++ src/components/CCombobox.vue
@@ -2,29 +2,42 @@
   <div class="combo">
     <div class="head">
       <img src="../assets/images/combobox/plus_white.png"/>
-      <span>ДОБАВИТЬ ТС</span>
-      <br/>
+      <span style="">ДОБАВИТЬ ТС</span>
+      <div style="clear: both; margin: 15px"></div>
       <img src="../assets/images/combobox/plus.png"/>
       <span>ДОБАВИТЬ ГРУППУ </span>
     </div>
-    <div class="combo-el" v-for="el in groups" :key="el.id">
-
-      <img v-if="~stateCombo.selected.indexOf(el.id)" src="../assets/images/combobox/on.png"/>
-      <img v-else src="../assets/images/combobox/off.png"/>
+    <div class="hr" style="margin-top: 5px"></div>
+    <div @click="showGroup(el)" class="combo-el" v-for="el in groups" :key="el.id">
+      <img @click.stop="onSelect(el)" class="img0" v-if="~stateCombo.selected.indexOf(el.id)"
+           src="../assets/images/combobox/on.png"/>
+      <img @click.stop="onSelect(el)" class="img0" v-else src="../assets/images/combobox/off.png"/>
 
       <img v-if="~stateCombo.opened.indexOf(el.id)" src="../assets/images/combobox/arrow-down.png"/>
       <img v-else src="../assets/images/combobox/arrow-right.png"/>
 
+      <span> {{el.name}} {{el.content? "("+el.content.length+")":"(0)"}}</span>
 
-      <span>
-        {{el.name}} {{el.content? "("+el.content.length+")":""}}
-      </span>
-      <!--<hr color="#ddd" size="1" width="90%" style="margin: 15px auto 0" />-->
-      <div style="height: 1px; width: 90%; background: #ddd; margin: 0 auto; margin-top: 15px"></div>
-      <div class="combo-el-sub" v-if="~stateCombo.opened.indexOf(el.id)" v-for="(item, index) in el.content"
-           :key="index">
-        <img v-if="~stateCombo.selected.indexOf(el.id+'_'+index)" src="../assets/images/combobox/on.png"/>
-        <img v-else src="../assets/images/combobox/off.png"/>
+      <div class="hr"></div>
+
+      <div @click.stop="showGroup()"
+           class="combo-item"
+           v-if="~stateCombo.opened.indexOf(el.id)"
+           v-for="(item, ind) in el.content" :key="ind">
+        <img class="img0"
+             v-if="~stateCombo.selectedItem.indexOf(el.id+'_'+item.name)"
+             @click.stop="onSelect(el, item)"
+             src="../assets/images/combobox/on.png"/>
+        <img class="img0"
+             v-else
+             @click.stop="onSelect(el, item)"
+             src="../assets/images/combobox/off.png"/>
+
+        <img class="img0" v-if="item.alarm" src="../assets/images/combobox/elem-normal.png"/>
+        <img class="img0" v-else src="../assets/images/combobox/elem-signal.png"/>
+
+        <img v-if="item.stoped" src="../assets/images/combobox/p.png"/>
+
         <span>{{item.name}}</span>
       </div>
     </div>
@@ -39,7 +52,8 @@
       return {
         stateCombo: {
           opened: [2],
-          selected: [1, 2, 3, 4, 7]
+          selected: [1, 2, 3, 4, 7],
+          selectedItem: ["1_Авто 6", "1_Авто 4", "2_Авто 5", "3_Авто 8"]
         },
         groups: [
           {
@@ -71,11 +85,9 @@
               alarm: true
             }, {
               name: "Авто6",
-              stoped: true,
               alarm: false
             }, {
               name: "Авто 7",
-              stoped: true,
               alarm: false
             }]
           }, {
@@ -104,13 +116,38 @@
         ]
       }
     },
-    // computed: {
-    //   isOpen
-    // },
-    mounted() {
-
-      // window.DDD = this.stateCombo
-
+    methods: {
+      showGroup(el) {
+        if (el) {
+          let place = this.stateCombo.opened.indexOf(el.id);
+          if (place > -1) this.stateCombo.opened.splice(place, 1);
+          else this.stateCombo.opened.push(el.id);
+        }
+      },
+      onSelect(el, item) {
+        let groups = this.stateCombo.selected;
+        let items = this.stateCombo.selectedItem;
+        if (!item) {
+          let place = groups.indexOf(el.id);
+          (place > -1) ? groups.splice(place, 1) : groups.push(el.id);
+          (place > -1) ? this.unSelectAllItems(el) : this.selectAllItems(el);
+        } else {
+          let newid = el.id + "_" + item.name;
+          let place = items.indexOf(newid);
+          (place > -1) ? items.splice(place, 1) : items.push(newid);
+        }
+      },
+      unSelectAllItems(el) {
+        this.stateCombo.selectedItem = this.stateCombo.selectedItem.filter(select => select.indexOf(el.id) !== 0);
+      },
+      selectAllItems(el) {
+        this.groups.map(group => {
+          group.id === el.id && group.content.map(q => {
+            let newid = el.id + '_' + q.name;
+            if (this.stateCombo.selectedItem.indexOf(newid) !== 0) this.stateCombo.selectedItem.push(newid);
+          })
+        });
+      }
     }
   }
 </script>
@@ -126,7 +163,7 @@
   .combo img {
     cursor: pointer;
     position: relative;
-    top:6px;
+    top: 6px;
   }
 
   .head {
@@ -137,16 +174,13 @@
     padding: 20px 26px;
     vertical-align: top;
   }
-  .head span{
-    padding: 15px 0;
+
+  .head span, .combo-item span {
+    padding-left: 10px;
   }
 
   .combo-el {
-    /*margin: 5px 0;*/
-    /*line-height: 30px;*/
-    /*padding-bottom: 10px;*/
     padding: 5px 0 0px;
-
 
   }
 
@@ -154,22 +188,24 @@
     background: #f5f5f5;
   }
 
-  /*.combo-el img {*/
-    /*position: relative;*/
-    /*top:6px;*/
-  /*}*/
-
-  .combo-el span, .combo-el-sub span {
-    /*vertical-align: top;*/
+  .combo-el .img0 {
+    padding-left: 28px;
   }
 
-  .combo-el-sub:hover {
+  .combo-item:hover {
     border: 1px solid red;
     margin: -1px;
   }
 
-  .combo-el-sub {
-    padding: 12px 0px;
+  .combo-item {
+    padding: 7px 0px 12px;
   }
 
+  .hr {
+    height: 1px;
+    width: 90%;
+    background: #ddd;
+    margin: 0 auto;
+    margin-top: 15px
+  }
 </style>
